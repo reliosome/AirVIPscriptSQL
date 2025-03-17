02d9a683921373fe857ba3a7007d0dffd7c28987
reliosome <aurele.a.collin@gmail.com>
2025-03-17 15:06:49 -0400

    modifié table aeroport

--- airVIPcreationTables.sql
+++ airVIPcreationTables.sql
@@ -1,10 +1,11 @@
 -- ====================================================
 -- Création Table aéroport
 -- ====================================================
-CREATE TABLE [dbo].[Aéroport] (
-    [code_IATA] INT         IDENTITY (1, 1) NOT NULL,
-    [ville]     VARCHAR (1) NOT NULL,
-    [pays]      VARCHAR (1) NOT NULL,
+CREATE TABLE [dbo].[Aeroport] (
+    [code_IATA]         INT         IDENTITY (1, 1) NOT NULL,
+    [ville]             VARCHAR (1) NOT NULL,
+    [pays]              VARCHAR (1) NOT NULL,
+    [distance_montreal] INT         NOT NULL,
     CONSTRAINT [PK_Aéroport] PRIMARY KEY CLUSTERED ([code_IATA] ASC)
 );
 
