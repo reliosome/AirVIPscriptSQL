b2f04d6a66489a572fd771af3ea35b8eaafddc74
NathanMarceus <nathan.marceus.1@ens.etsmtl.ca>
2025-03-19 11:42:42 -0400

    Update airVIPcreationTables.sql
    création de la table Utilisateur

--- airVIPcreationTables.sql
+++ airVIPcreationTables.sql
@@ -9,3 +9,12 @@ CREATE TABLE [dbo].[Aeroport] (
     CONSTRAINT [PK_Aéroport] PRIMARY KEY CLUSTERED ([code_IATA] ASC)
 );
 
+CREATE TABLE [dbo].[Utilisateur] (
+    [id_utilisateur] INT          IDENTITY (1, 1) NOT NULL,
+    [prenom]         VARCHAR (25) NOT NULL,
+    [nom]            VARCHAR (25) NOT NULL,
+    [mot_de_passe]   VARCHAR (25) NOT NULL,
+    CONSTRAINT [PK_Utilisateur] PRIMARY KEY CLUSTERED ([id_utilisateur] ASC)
+);
+
+
