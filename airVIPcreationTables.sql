92bae8a22c779574bd0fe2e128c028086b92eb86
NathanMarceus <nathan.marceus.1@ens.etsmtl.ca>
2025-03-19 16:24:29 -0400

    création table administrateur

--- airVIPcreationTables.sql
+++ airVIPcreationTables.sql
@@ -12,7 +12,6 @@ CREATE TABLE [dbo].[Aeroport] (
 -- ====================================================
 -- Création Table Utilisateur
 -- ====================================================
-
 CREATE TABLE [dbo].[Utilisateur] (
     [id_utilisateur] INT          IDENTITY (1, 1) NOT NULL,
     [prenom]         VARCHAR (25) NOT NULL,
@@ -20,11 +19,9 @@ CREATE TABLE [dbo].[Utilisateur] (
     [mot_de_passe]   VARCHAR (25) NOT NULL,
     CONSTRAINT [PK_Utilisateur] PRIMARY KEY CLUSTERED ([id_utilisateur] ASC)
 );
-
 -- ====================================================
 -- Création Table Avion
 -- ====================================================
-
 CREATE TABLE [dbo].[Avion] (
     [avion_id] INT         IDENTITY (1, 1) NOT NULL,
     [image]    VARCHAR (1) NOT NULL,
@@ -36,7 +33,6 @@ CREATE TABLE [dbo].[Avion] (
 -- ====================================================
 -- Création Table Client
 -- ====================================================
-
 CREATE TABLE [dbo].[Client] (
     [Id_client]      INT IDENTITY (1, 1) NOT NULL,
     [telephone]      INT NOT NULL,
@@ -44,5 +40,18 @@ CREATE TABLE [dbo].[Client] (
     CONSTRAINT [PK_Client] PRIMARY KEY CLUSTERED ([Id_client] ASC),
     CONSTRAINT [FK_Client_Utilisateur] FOREIGN KEY ([id_utilisateur]) REFERENCES [dbo].[Utilisateur] ([id_utilisateur])
 );
+
+-- ====================================================
+-- Création Table Client
+-- ====================================================
+CREATE TABLE [dbo].[Administrateur] (
+    [id_administrateur] INT          IDENTITY (1, 1) NOT NULL,
+    [titre_de_poste]    VARCHAR (25) NOT NULL,
+    [id_utilisateur]    INT          NOT NULL,
+    CONSTRAINT [PK_Administrateur] PRIMARY KEY CLUSTERED ([id_administrateur] ASC),
+    CONSTRAINT [FK_Administrateur_Utilisateur] FOREIGN KEY ([id_utilisateur]) REFERENCES [dbo].[Utilisateur] ([id_utilisateur])
+);
+
+
 
 
