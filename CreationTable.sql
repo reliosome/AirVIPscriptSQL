8d6811ac6911af6646a204f4670c489f115f0a7b
Nico <nicolas.berlinguet.1@ens.etsmtl.ca>
2025-03-19 10:01:38 -0400

    "rajout de tableAvion

--- CreationTable.sql
+++ CreationTable.sql
@@ -9,3 +9,18 @@ CREATE TABLE [dbo].[Aeroport] (
     CONSTRAINT [PK_Aéroport] PRIMARY KEY CLUSTERED ([code_IATA] ASC)
 );
 
+-- ====================================================
+-- Création Table Avion
+-- ====================================================
+
+CREATE TABLE [dbo].[Avion] (
+    [avion_id] INT         IDENTITY (1, 1) NOT NULL,
+    [image]    VARCHAR (1) NOT NULL,
+    [modele]   VARCHAR (1) NOT NULL,
+    [capacite] INT         NOT NULL,
+    CONSTRAINT [PK_Avion] PRIMARY KEY CLUSTERED ([avion_id] ASC)
+);
+
+
+
+
