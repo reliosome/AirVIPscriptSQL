98b2fd3e3d5ae429afb3a71379961275a8ba4fe8
NathanMarceus <nathan.marceus.1@ens.etsmtl.ca>
2025-03-19 11:49:01 -0400

    merge de la branche master et main

--- airVIPcreationTables.sql
+++ airVIPcreationTables.sql
@@ -9,6 +9,10 @@ CREATE TABLE [dbo].[Aeroport] (
     CONSTRAINT [PK_Aéroport] PRIMARY KEY CLUSTERED ([code_IATA] ASC)
 );
 
+-- ====================================================
+-- Création Table Utilisateur
+-- ====================================================
+
 CREATE TABLE [dbo].[Utilisateur] (
     [id_utilisateur] INT          IDENTITY (1, 1) NOT NULL,
     [prenom]         VARCHAR (25) NOT NULL,
@@ -17,4 +21,14 @@ CREATE TABLE [dbo].[Utilisateur] (
     CONSTRAINT [PK_Utilisateur] PRIMARY KEY CLUSTERED ([id_utilisateur] ASC)
 );
 
+-- ====================================================
+-- Création Table Avion
+-- ====================================================
 
+CREATE TABLE [dbo].[Avion] (
+    [avion_id] INT         IDENTITY (1, 1) NOT NULL,
+    [image]    VARCHAR (1) NOT NULL,
+    [modele]   VARCHAR (1) NOT NULL,
+    [capacite] INT         NOT NULL,
+    CONSTRAINT [PK_Avion] PRIMARY KEY CLUSTERED ([avion_id] ASC)
+);
