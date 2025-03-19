83ef3fcb73a72a330fb43ece3c0ff6ccf894f121
NathanMarceus <nathan.marceus.1@ens.etsmtl.ca>
2025-03-19 16:54:42 -0400

    script de suppression de tables

---
+++ airVIPsupprimerTables.sql
@@ -0,0 +1,17 @@
+-- Suppression des données des utilisateurs et des tables dans la base de données
+DELETE FROM Client;
+DROP TABLE Client;
+
+DELETE FROM Administrateur;
+DROP TABLE Administrateur;
+
+DELETE FROM Utilisateur;
+DROP TABLE Utilisateur;
+
+
+-- Suppression des données des aéroports et des tables dans la base de données
+DELETE FROM Avion;
+DROP TABLE Avion;
+
+DELETE FROM Aeroport;
+DROP TABLE Aeroport;
