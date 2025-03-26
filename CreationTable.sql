f80ea5a716521b6db5bc0b422f9d9f4c6687d583
Nico <nicolas.berlinguet.1@ens.etsmtl.ca>
2025-03-26 09:22:25 -0400

    Table Vol

--- CreationTable.sql
+++ CreationTable.sql
@@ -21,6 +21,21 @@ CREATE TABLE [dbo].[Avion] (
     CONSTRAINT [PK_Avion] PRIMARY KEY CLUSTERED ([avion_id] ASC)
 );
 
+CREATE TABLE [dbo].[Vol] (
+    [vol_id]              INT         NOT NULL,
+    [prix]                FLOAT (53)  NOT NULL,
+    [modelAvion]          VARCHAR (1) NOT NULL,
+    [disponibilite]       VARCHAR (1) NOT NULL,
+    [nb_place]            INT         NOT NULL,
+    [FK__Aeroport_arrive] INT         NOT NULL,
+    [FK_Aeroport_depart]  INT         NOT NULL,
+    [FK_avion]            INT         NOT NULL,
+    CONSTRAINT [PK_Vol] PRIMARY KEY CLUSTERED ([vol_id] ASC),
+    CONSTRAINT [AeroportArrivee] FOREIGN KEY ([FK__Aeroport_arrive]) REFERENCES [dbo].[Aeroport] ([id_aeroport]),
+    CONSTRAINT [AeroportDepart] FOREIGN KEY ([FK_Aeroport_depart]) REFERENCES [dbo].[Aeroport] ([id_aeroport]),
+    CONSTRAINT [Avion_du_vol] FOREIGN KEY ([FK_avion]) REFERENCES [dbo].[Avion] ([avion_id])
+);
+
 
 
 
